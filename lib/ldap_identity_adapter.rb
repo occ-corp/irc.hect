6412b71c6cde22a4d638b911b2c2d1bb58f1aa16
Tomohiro TAIRA <tomohiro@occ.co.jp>
2013-03-14 13:37:06 +0900

    add ldap authentication adapter

---
+++ lib/ldap_identity_adapter.rb
@@ -0,0 +1,39 @@
+require 'ldap'
+
+module Hector
+  class LdapIdentityAdapter
+    def authenticate(username, password)
+      yield ldap_login(username, password)
+    end
+
+    def remember(username, password)
+      false
+    end
+
+    def forget(username)
+      false
+    end
+
+    def normalize(username)
+      username.strip.downcase
+    end
+
+    private
+      def ldap_login(username, password)
+        ldap_server = ENV['LDAP_SERVER']
+        ldap_domain = ENV['LDAP_DOMAIN']
+
+        dn="uid=#{username},ou=Users,#{ldap_domain}"
+
+        conn = LDAP::Conn.new(ldap_server, LDAP::LDAP_PORT)
+        conn.set_option(LDAP::LDAP_OPT_PROTOCOL_VERSION, 3)
+
+        conn.bind(dn, password)
+        conn.unbind
+        return true
+      rescue LDAP::ResultError => e
+        puts "Ldap authentication error. #{e.to_s}. account => #{username}"
+        return false
+      end
+  end
+end
