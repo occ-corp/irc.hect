4e205698c521418576171d248a420b220db457fe
Tomohiro TAIRA <tomohiro@occ.co.jp>
2013-03-14 13:37:19 +0900

    add hector configuration script

---
+++ init.rb
@@ -0,0 +1,6 @@
+require_relative 'lib/ldap_identity_adapter'
+
+Hector.server_name = ENV['IRC_SERVER']
+Hector.port = 6667
+Hector.ssl_port = 6668
+Hector::Identity.adapter = Hector::LdapIdentityAdapter.new
